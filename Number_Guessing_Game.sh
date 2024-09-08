46f7fd6ec5f781eb57c150223d70aeed50b0795d
fr3dy5 <fr3dy5al@gmail.com>
2024-09-08 01:48:48 -0500

    its a start

---
+++ Number_Guessing_Game.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Welcome to the Number Guessing Game!"
+
+echo "Guess the number (1-10) or enter '0' to quit:($attempts)"
+
+echo "Too low! Try again."
+
+echo "Too high! Try again."
+
+echo "Thanks for playing. Goodbye!"
+
+echo "Congratulations! You the guessed the correct number in ($attempts) attempts."
