b1737a524d780853a2af25f29634f37351e70587
fr3dy5 <fr3dy5al@gmail.com>
2024-09-08 02:05:24 -0500

    hope im getting warm

--- Number_Guessing_Game.sh
+++ Number_Guessing_Game.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+input=$guess
+$random=$secret_number
+#$attempts=input+1#
+
 echo "Welcome to the Number Guessing Game!"
 
 echo "Guess the number (1-10) or enter '0' to quit:($attempts)"
-
+if [["$guess" -lt "$secret_number"]]; then
 echo "Too low! Try again."
+fi
 
-echo "Too high! Try again."
+if [[ "$guess" -gt "$secret_number"]]; then
+    echo "Too high! Try again."
+fi
 
-echo "Thanks for playing. Goodbye!"
+if [[ "$guess" -eq "0"]]; then
+    echo "Thanks for playing. Goodbye!"
+fi
 
-echo "Congratulations! You the guessed the correct number in ($attempts) attempts."
+if [[ "$guess" -eq "$secret_number"]]; then
+    echo "Congratulations! You the guessed the correct number in ($attempts) attempts."
+fi
