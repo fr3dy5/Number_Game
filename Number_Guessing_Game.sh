3dadd63204c249041af7eb74af90b206fe86fd66
fr3dy5 <fr3dy5al@gmail.com>
2024-09-08 18:25:42 -0500

    restructure, add variables, and de-redundisize

--- Number_Guessing_Game.sh
+++ Number_Guessing_Game.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-input=$guess
-$random=$secret_number
-#$attempts=input+1#
+read -p "Please enter a number:" guess
+
+attempts=1
+secret_number= $((RANDOM % 10 + 1))
+attempts=$attempts+1
+winner=echo "Congratulations! You the guessed the correct number in ($attempts) attempts."
+later=echo "Thanks for playing. Goodbye!"
+
+echo "Guess the number (1-10) or enter '0' to quit:($attempts)"
 
 echo "Welcome to the Number Guessing Game!"
 
-echo "Guess the number (1-10) or enter '0' to quit:($attempts)"
-if [["$guess" -lt "$secret_number"]]; then
-echo "Too low! Try again."
-fi
-
-if [[ "$guess" -gt "$secret_number"]]; then
-    echo "Too high! Try again."
-fi
-
-if [[ "$guess" -eq "0"]]; then
-    echo "Thanks for playing. Goodbye!"
-fi
-
-if [[ "$guess" -eq "$secret_number"]]; then
-    echo "Congratulations! You the guessed the correct number in ($attempts) attempts."
-fi
+until [["$guess" -eq "0"]]; then
+    later;
+        elif [["$guess" -eq "secret_number"]]; then
+    winner;
+        if [["$guess" -lt "$secret_number"]]; then
+            echo "Too low! Try again.";
+        if [[ "$guess" -gt "$secret_number"]]; then
+            echo "Too high! Try again.";
+done
