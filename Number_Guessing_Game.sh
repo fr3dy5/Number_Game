84b8b7f6f1bb0ce97389532c5fe33d049a82c0f9
fr3dy5 <fr3dy5al@gmail.com>
2024-09-08 22:49:25 -0500

    idk im gonna throw this at the wall now

--- Number_Guessing_Game.sh
+++ Number_Guessing_Game.sh
@@ -5,8 +5,8 @@ echo "Guess the number (1-10) or enter '0' to quit:($attempts)"
 read -p "Please enter a number:" guess
 
 attempts=1
-secret_number=$((RANDOM % 10 ) + 1)
-attempts=(($attempts) + 1)
+secret_number=$((RANDOM%10)+1)
+attempts=(($attempts)+1)
 winner=echo "Congratulations! You the guessed the correct number in ($attempts) attempts."
 later=echo "Thanks for playing. Goodbye!"
 
@@ -19,3 +19,13 @@ if [["$guess" -eq "0"]]; then
     else [["$guess" -eq "secret_number"]]; then
         winner;
 done
+
+##./Number_Guessing_Game.sh: command substitution: line 8: syntax error near unexpected token `+'
+#./Number_Guessing_Game.sh: command substitution: line 8: `(RANDOM % 10 ) + 1'
+#./Number_Guessing_Game.sh: line 9: syntax error near unexpected token `('
+#./Number_Guessing_Game.sh: line 9: `attempts=(($attempts) + 1)'
+#./Number_Guessing_Game.sh: line 10: Congratulations! You the guessed the correct number in (1) attempts.: command not found
+#./Number_Guessing_Game.sh: line 11: Thanks for playing. Goodbye!: command not found
+#./Number_Guessing_Game.sh: line 17: syntax error in conditional expression: unexpected token `;'
+#./Number_Guessing_Game.sh: line 17: syntax error near `;'
+#./Number_Guessing_Game.sh: line 17: `    elif [[ "$guess" -gt "$secret_number"]]; then'
