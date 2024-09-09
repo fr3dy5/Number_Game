b2a97876c4b30072d065f7439c673a299c718c9f
fr3dy5 <fr3dy5al@gmail.com>
2024-09-08 20:02:29 -0500

    restructured some more and editted more errors. here we go

--- Number_Guessing_Game.sh
+++ Number_Guessing_Game.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
+echo "Welcome to the Number Guessing Game!"
+echo "Guess the number (1-10) or enter '0' to quit:($attempts)"
 read -p "Please enter a number:" guess
 
-attempts=1
-secret_number= $((RANDOM % 10 + 1))
-attempts=$attempts+1
-winner=echo "Congratulations! You the guessed the correct number in ($attempts) attempts."
-later=echo "Thanks for playing. Goodbye!"
-
-echo "Guess the number (1-10) or enter '0' to quit:($attempts)"
-
-echo "Welcome to the Number Guessing Game!"
+attempts= 1
+secret_number= $((RANDOM % 10 ) + 1)
+attempts= (($attempts) + 1)
+winner= echo "Congratulations! You the guessed the correct number in ($attempts) attempts."
+later= echo "Thanks for playing. Goodbye!"
 
 until [["$guess" -eq "0"]]; then
     later;
+    done
         elif [["$guess" -eq "secret_number"]]; then
-    winner;
-        if [["$guess" -lt "$secret_number"]]; then
+            winner;
+        elif [["$guess" -lt "$secret_number"]]; then
             echo "Too low! Try again.";
-        if [[ "$guess" -gt "$secret_number"]]; then
+        else [[ "$guess" -gt "$secret_number"]]; then
             echo "Too high! Try again.";
 done
